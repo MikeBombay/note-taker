a1ee32913417f6d191a1d1d77eb5734b434bb09f
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-08 21:52:15 -0400

    commit for updates

--- server.js
+++ server.js
@@ -1,46 +1,19 @@
 //dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
-const db = require("./db/db")
-const router = require("express").Router();
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
-
-//initialize app, data parsing
+//initialize serbver
 const app = express();
 const PORT = process.env.PORT || 3005;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-//push new note 
-app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    notes.push(newNote);
-    updateDb();
-    return console.log("Added note: "+newNote.title);
-});
-
-//updates db file.json file
-function updateDb() {
-    fs.writeFile("db/db.json", JSON.stringify(notes), function (err) {
-        if (err) {
-            console.log("error")
-            return console.log(err);
-        }})};
-
-
-
-//Routes
-
-router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //listener
 app.listen(PORT, function () {
