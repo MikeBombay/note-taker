35dd5216f408932262096b85dbb08f618203a8b4
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-08-10 23:34:01 -0400

    delete and things

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,8 +10,8 @@ router.get("/notes", (req, res) => {
 
 //post new note to page, assign id, pushes to variable and writes to db
 //**additional note: I installed uuid and it seems to be automatically applying unique IDs 
-//**to my notes, which seems to be over-riding what I have coded here, I have left it 
-//* this way as it better reflects requirements
+//**to my notes, which seems to be over-riding what I have coded here, this seems like a
+//*good problem to have so I lef it in
 router.post("/notes", (req, res) => {
   let newNote = req.body;
   newNote.id = notes.length.toString();
@@ -20,15 +20,15 @@ router.post("/notes", (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
   return res.json(notes);
 });
-
-  //deletes note
-router.delete("/notes/:id", (req, res) => {
-  let deleteNote = req.params.id;
-  let arr = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //new array created without ID
-  let arr2 = arr.filter((file) => file.id !== deleteNote);
-  fs.writeFileSync("./db/db.json", JSON.stringify(newArr));
-  return res.json(arr2);
-});
+ //deletes note
+ router.delete("/notes/:id", (req, res) => {
+    let deleteNote = req.params.id;
+    let arr = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    //new array created/written without 
+    let arr2 = arr.filter((file) => file.id !== deleteNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(arr2));
+    return res.json(arr2);
+  });
+ 
 
 module.exports = router;
